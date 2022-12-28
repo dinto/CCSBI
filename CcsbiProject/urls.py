687ccb5228d92982cbbb6542dec1c247217f8692
dinto.t.d <dinto.t.d@gmail.com>
2022-12-28 15:16:03 +0530

    project page

--- CcsbiProject/urls.py
+++ CcsbiProject/urls.py
@@ -3,5 +3,19 @@ from CcsbiProject import views
 
 urlpatterns = [
     path('', views.CcsbiProject, name='index'),
+    path('home', views.CcsbiProject, name='index'),
     path('contact', views.contact, name='contact'),
+    path('projects', views.project, name='project'),
+    path('Networks', views.Networks, name='Networks'),
+    path('Datasets', views.Datasets, name='Datasets'),
+    path('Resources', views.Resources, name='Resources'),
+    path('Capacity', views.Capacity, name='Capacity'),
+    path('Events', views.Events, name='Events'),  
+    path('Opportunities', views.Opportunities, name='Opportunities'),
+    
+
+
+
+
+
 ]
--- CcsbiProject/views.py
+++ CcsbiProject/views.py
@@ -6,3 +6,19 @@ def CcsbiProject(request):
 
 def contact(request):
     return render(request,'contact.html',{})
+
+def project(request):
+    return render(request,'Projects.html',{})
+
+def Networks(request):
+    return render(request,'Networks.html',{})
+def Datasets(request):
+    return render(request,'Datasets.html',{})
+def Resources(request):
+    return render(request,'Resources.html',{})
+def Capacity(request):
+    return render(request,'Capacity.html',{})
+def Events(request):
+    return render(request,'Events.html',{})
+def Opportunities(request):
+    return render(request,'Opportunities.html',{})
