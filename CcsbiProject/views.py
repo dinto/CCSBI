5c49c9fa58a8a4ba757d5fda8f18f38df646ad6a
dinto.t.d <dinto.t.d@gmail.com>
2023-02-02 17:05:36 +0530

    active set

--- CcsbiProject/views.py
+++ CcsbiProject/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 def CcsbiProject(request):
-    return render(request,'index.html',{})
+    return render(request,'index.html',{'navbar':'home'})
 
 def contact(request):
-    return render(request,'contact.html',{})
+    return render(request,'contact.html',{'navbar':'contact'})
 
 def project(request):
-    return render(request,'Projects.html',{})
+    return render(request,'Projects.html',{'navbar':'project'})
 
 def Networks(request):
-    return render(request,'Networks.html',{})
+    return render(request,'Networks.html',{'navbar':'network'})
 def Datasets(request):
-    return render(request,'Datasets.html',{})
+    return render(request,'Datasets.html',{'navbar':'dataset'})
 def Resources(request):
-    return render(request,'Resources.html',{})
+    return render(request,'Resources.html',{'navbar':'resource'})
 def Capacity(request):
-    return render(request,'Capacity.html',{})
+    return render(request,'Capacity.html',{'navbar':'capacity'})
 def Events(request):
-    return render(request,'Events.html',{})
+    return render(request,'Events.html',{'navbar':'event'})
 def Opportunities(request):
-    return render(request,'Opportunities.html',{})
+    return render(request,'Opportunities.html',{'navbar':'opportunities'})
 def Event1(request):
-    return render(request,'Event1.html',{})
+    return render(request,'testactive.html',{'navbar':'event'})
